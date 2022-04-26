7e60106ade564a7f699b1f85b287262ad73ab1a6
Mathew <mrk14994@gmail.com>
2022-04-26 13:16:33 +0530

    Update IncomingCallNotificationService.java

--- android/src/main/java/federico/amura/flutter_twilio/IncomingCallNotificationService.java
+++ android/src/main/java/federico/amura/flutter_twilio/IncomingCallNotificationService.java
@@ -94,10 +94,11 @@ public class IncomingCallNotificationService extends Service {
         Log.e(TAG, "****************************accept start****************");
         Log.e(TAG, "Accept call invite. App visible: " + isAppVisible() + ". Locked: " + isLocked());
         this.stopServiceIncomingCall();
-        Log.e(TAG, "****************************accept**************** !isLocked() && isAppVisible()");
-        // Inform call accepted
-        Log.e(TAG, "Answering from APP");
-        this.informAppAcceptCall(callInvite);
+
+        Log.e(TAG, "****************************accept**************** else part");
+        Log.e(TAG, "Answering from custom UI");
+        Log.e(TAG, "Answering from call Invite "+ callInvite.getCallSid());
+        this.openBackgroundCallActivityForAcceptCall(callInvite);
         Log.e(TAG, "****************************accept end****************");
     }
 
