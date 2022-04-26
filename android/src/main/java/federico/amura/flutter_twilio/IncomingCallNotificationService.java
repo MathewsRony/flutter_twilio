1ab6b9dc0f75528762c0222d209c35c9aba54203
Mathew <mrk14994@gmail.com>
2022-04-26 10:56:36 +0530

    test 1.4

--- android/src/main/java/federico/amura/flutter_twilio/IncomingCallNotificationService.java
+++ android/src/main/java/federico/amura/flutter_twilio/IncomingCallNotificationService.java
@@ -91,22 +91,13 @@ public class IncomingCallNotificationService extends Service {
     }
 
     private void accept(CallInvite callInvite) {
-        Log.e(TAG, "****************************accept start****************");
-        Log.e(TAG, "Accept call invite. App visible: " + isAppVisible() + ". Locked: " + isLocked());
-        this.stopServiceIncomingCall();
-
-        if (!isLocked() && isAppVisible()) {
-            Log.e(TAG, "****************************accept**************** !isLocked() && isAppVisible()");
-            // Inform call accepted
-            Log.e(TAG, "Answering from APP");
-            this.informAppAcceptCall(callInvite);
-        } else {
-            Log.e(TAG, "****************************accept**************** else part");
-            Log.e(TAG, "Answering from custom UI");
-            Log.e(TAG, "Answering from call Invite "+ callInvite.getCallSid());
-            this.openBackgroundCallActivityForAcceptCall(callInvite);
-        }
-        Log.e(TAG, "****************************accept end****************");
+        Log.i(TAG, "accept call invite!");
+        Intent activeCallIntent = new Intent();
+        activeCallIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        activeCallIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activeCallIntent.putExtra(TwilioConstants.EXTRA_INCOMING_CALL_INVITE, callInvite);
+        activeCallIntent.setAction(TwilioConstants.ACTION_ACCEPT);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(activeCallIntent);
     }
 
     private void reject(CallInvite callInvite) {
--- android/src/main/java/federico/amura/flutter_twilio/Utils/NotificationUtils.java
+++ android/src/main/java/federico/amura/flutter_twilio/Utils/NotificationUtils.java
@@ -76,7 +76,7 @@ public class NotificationUtils {
                 0,
                 intent,
                 Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ?
-                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE : PendingIntent.FLAG_UPDATE_CURRENT
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE : PendingIntent.FLAG_UPDATE_CURRENT
         );
 
 
@@ -92,7 +92,7 @@ public class NotificationUtils {
                 0,
                 rejectIntent,
                 Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ?
-                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE : PendingIntent.FLAG_UPDATE_CURRENT
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE : PendingIntent.FLAG_UPDATE_CURRENT
         );
 
         // Accept intent
@@ -107,23 +107,28 @@ public class NotificationUtils {
                 0,
                 acceptIntent,
                 Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ?
-                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE : PendingIntent.FLAG_UPDATE_CURRENT
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE : PendingIntent.FLAG_UPDATE_CURRENT
         );
 
         // Notification
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, createChannel(context, showHeadsUp))
-        .setSmallIcon(R.drawable.ic_phone_call)
-                .setContentTitle(notificationTitle)
-                .setContentText(notificationTitle)
-                .setCategory(Notification.CATEGORY_CALL)
-                .setFullScreenIntent(pendingIntent, true)
-                .setExtras(extras)
-                .setVibrate(new long[]{0, 400, 400, 400, 400, 400, 400, 400})
-                .setAutoCancel(true)
-                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                .addAction(android.R.drawable.ic_menu_delete, context.getString(R.string.btn_accept), piRejectIntent)
-                .addAction(android.R.drawable.ic_menu_call, context.getString(R.string.btn_accept), piAcceptIntent)
-                .setFullScreenIntent(pendingIntent, true);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, createChannel(context, showHeadsUp));
+        builder.setSmallIcon(R.drawable.ic_phone_call);
+        builder.setContentTitle(notificationTitle);
+        builder.setContentText(notificationText);
+        builder.setCategory(NotificationCompat.CATEGORY_CALL);
+        builder.setAutoCancel(true);
+        builder.setExtras(extras);
+        builder.setVibrate(new long[]{0, 400, 400, 400, 400, 400, 400, 400});
+        builder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+//        if (!(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) || isAppVisible())
+        builder.addAction(android.R.drawable.ic_menu_delete, context.getString(R.string.btn_reject), piRejectIntent);
+//        if (!(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) || isAppVisible())
+        builder.addAction(android.R.drawable.ic_menu_call, context.getString(R.string.btn_accept), piAcceptIntent);
+        builder.setFullScreenIntent(pendingIntent, true);
+        builder.setColor(Color.rgb(20, 10, 200));
+        builder.setOngoing(true);
+        builder.setPriority(NotificationCompat.PRIORITY_MAX);
+        builder.setContentIntent(pendingIntent);
         return builder.build();
     }
 
