e2633984c391f910671dae1d8b0adbb058547523
Mathew <mrk14994@gmail.com>
2022-05-05 15:48:26 +0530

    Update SwiftFlutterTwilioPlugin.swift

--- ios/Classes/SwiftFlutterTwilioPlugin.swift
+++ ios/Classes/SwiftFlutterTwilioPlugin.swift
@@ -452,7 +452,6 @@ public class SwiftFlutterTwilioPlugin: NSObject, FlutterPlugin,   NotificationDe
 
 
         self.callInvite?.reject()
-        self.callInvite = nil
         audioDevice.isEnabled = true
         performEndCallAction(uuid: self.callInvite!.uuid)
         self.incomingPushHandled()
@@ -637,26 +636,26 @@ public class SwiftFlutterTwilioPlugin: NSObject, FlutterPlugin,   NotificationDe
         self.result = nil
 
         self.callDisconnected(id: uuid, error: nil)
-//         let endCallAction = CXEndCallAction(call: uuid)
-//         let transaction = CXTransaction(action: endCallAction)
-//
-//         callKitCallController.request(transaction) { error in
-//             if error != nil {
-//                 NSLog("Error ending call:")
-//                 self.result?(FlutterError.init(
-//                     code: "Error",
-//                     message: "Error",
-//                     details: "Error"
-//                 ))
-//
-//                 self.result = nil
-//             } else {
-//                 self.call = nil
-//                 self.callInvite = nil
-//                 self.result?("")
-//                 self.result = nil
-//             }
-//         }
+         let endCallAction = CXEndCallAction(call: uuid)
+         let transaction = CXTransaction(action: endCallAction)
+
+         callKitCallController.request(transaction) { error in
+             if error != nil {
+                 NSLog("Error ending call:")
+                 self.result?(FlutterError.init(
+                     code: "Error",
+                     message: "Error",
+                     details: "Error"
+                 ))
+
+                 self.result = nil
+             } else {
+                 self.call = nil
+                 self.callInvite = nil
+                 self.result?("")
+                 self.result = nil
+             }
+         }
     }
     
     func performVoiceCall(uuid: UUID, completionHandler: @escaping (Bool) -> Swift.Void) {
