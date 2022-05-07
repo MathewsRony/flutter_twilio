5f952abe484436cca51775bef3c27190125004f9
Mathew <mrk14994@gmail.com>
2022-05-07 16:05:48 +0530

    Update SwiftFlutterTwilioPlugin.swift

--- ios/Classes/SwiftFlutterTwilioPlugin.swift
+++ ios/Classes/SwiftFlutterTwilioPlugin.swift
@@ -476,9 +476,9 @@ public class SwiftFlutterTwilioPlugin: NSObject, FlutterPlugin,   NotificationDe
 
             let title = userName ?? UserDefaults.standard.string(forKey: "_defaultDisplayName") ?? from ?? ""
             NSLog("!!!!!!! title !!!!!!!!")
-            NSLog("!!!!!!! "+title??"zxc"+" !!!!!!!!")
-            NSLog("!!!!!!! "+from??"asd"+" !!!!!!!!")
-            NSLog("!!!!!!! "+to??"qwe"+" !!!!!!!!")
+            NSLog("!!!!!!! "+title+" !!!!!!!!")
+            NSLog(from!)
+            NSLog(to!)
             content.title = String(format:  NSLocalizedString("Missed Call", comment: ""),title)
 
             let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
