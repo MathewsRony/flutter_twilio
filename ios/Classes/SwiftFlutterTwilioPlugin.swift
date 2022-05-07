3bdd8b23dd02913335299fc04cdfff9074f417d3
Mathew <mrk14994@gmail.com>
2022-05-07 17:15:34 +0530

    Update SwiftFlutterTwilioPlugin.swift

--- ios/Classes/SwiftFlutterTwilioPlugin.swift
+++ ios/Classes/SwiftFlutterTwilioPlugin.swift
@@ -478,7 +478,7 @@ public class SwiftFlutterTwilioPlugin: NSObject, FlutterPlugin,   NotificationDe
             NSLog("!!!!!!! title !!!!!!!!")
             NSLog("!!!!!!! "+title+" !!!!!!!!")
             NSLog(from!)
-            NSLog(callInvite.uuid.uuid)
+            NSLog(self.callInvite.uuid)
             NSLog(to!)
             content.title = String(format:  NSLocalizedString("Missed Call", comment: from!),from!)
             content
