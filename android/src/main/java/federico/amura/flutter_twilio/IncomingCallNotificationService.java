3ce13b84fe4a8bf1eb4cef027a38febc4f9e0a59
Mathew <mrk14994@gmail.com>
2022-07-25 16:46:59 +0530

    Update IncomingCallNotificationService.java

--- android/src/main/java/federico/amura/flutter_twilio/IncomingCallNotificationService.java
+++ android/src/main/java/federico/amura/flutter_twilio/IncomingCallNotificationService.java
@@ -40,6 +40,7 @@ public class IncomingCallNotificationService extends Service {
 
     private static final String TAG = IncomingCallNotificationService.class.getSimpleName();
 
+    private FlutterTwilioPlugin.CustomBroadcastReceiver broadcastReceiver;
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         String action = intent.getAction();
