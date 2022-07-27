848aa8b80c0e5d13fe0f7239a37a6397821f5fd0
Mathew <mrk14994@gmail.com>
2022-07-27 12:15:14 +0530

    Update NotificationUtils.java

--- android/src/main/java/federico/amura/flutter_twilio/Utils/NotificationUtils.java
+++ android/src/main/java/federico/amura/flutter_twilio/Utils/NotificationUtils.java
@@ -193,13 +193,10 @@ public class NotificationUtils {
 
         Intent acceptIntent = new Intent(context, IncomingCallNotificationService.class);
         acceptIntent.setAction(TwilioConstants.ACTION_MISSED_CALL);
+        returnCallIntent.putExtra(cancelledCallInvite.getTo(), "to");
+        returnCallIntent.putExtra(cancelledCallInvite.getFrom(), "callerId");
         acceptIntent.putExtra(TwilioConstants.EXTRA_INCOMING_CALL_INVITE, cancelledCallInvite);
-        acceptIntent.setFlags(
-                Intent.FLAG_ACTIVITY_NEW_TASK |
-                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
-                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK |
-                        Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
-        );
+
         Log.i("TAG", "Call canceled. buildMissedCallNotification 5 ");
         @SuppressLint("UnspecifiedImmutableFlag")
         PendingIntent pendingIntent = PendingIntent.getService(
