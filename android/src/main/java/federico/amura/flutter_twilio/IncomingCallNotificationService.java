ab63294c4c0e91b8e65b3b339753c954e9079e77
Mathew <mrk14994@gmail.com>
2022-08-01 16:39:22 +0530

    Revert "Update IncomingCallNotificationService.java"
    This reverts commit 3878bfef43048105c0fabdb920cc29f836a9f027.

--- android/src/main/java/federico/amura/flutter_twilio/IncomingCallNotificationService.java
+++ android/src/main/java/federico/amura/flutter_twilio/IncomingCallNotificationService.java
@@ -153,7 +153,8 @@ public class IncomingCallNotificationService extends Service {
 //
 //        Log.i(TAG, "From: " + cancelledCallInvite.getFrom() + ". To: " + cancelledCallInvite.getTo());
 //        this.informAppCancelCall();
-        FlutterTwilioPlugin.channel.invokeMethod("missedCall", "");
+        FlutterTwilioPlugin.responseChannel = new MethodChannel("flutter_twilio_response");
+        FlutterTwilioPlugin.responseChannel.invokeMethod("missedCall", "");
         stopForeground(true);
 //        Notification notification = NotificationUtils.createMissedCallNotification(getApplicationContext(), cancelledCallInvite, false);
 //        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
