6e8d82af9fd43df3bd05d3ef31569c8eee3f465a
Mathew <mrk14994@gmail.com>
2022-08-02 14:57:33 +0530

    Update NotificationUtils.java

--- android/src/main/java/federico/amura/flutter_twilio/Utils/NotificationUtils.java
+++ android/src/main/java/federico/amura/flutter_twilio/Utils/NotificationUtils.java
@@ -187,7 +187,7 @@ public class NotificationUtils {
         );
 
         Intent LaunchIntent = context.getPackageManager().getLaunchIntentForPackage("com.tch.crm");
-        LaunchIntent.setAction(TwilioConstants.RECEIVE_INTENT_EXAMPLE_ACTION);
+        LaunchIntent.setAction(TwilioConstants.ACTION_MISSED_CALL);
         LaunchIntent.putExtra("TwilioConstant", "cancelledCallInvite");
         LaunchIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         Log.i("TAG", "Call canceled. buildMissedCallNotification 5 ");
